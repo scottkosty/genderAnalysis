d28fdb1dee2e6d09b21db6558ec4259d9c471f7f
Scott Kostyshak <skostysh@princeton.edu>
2014-11-26 00:46:10 -0500

    Break up the title of plot to two lines

--- genderAnalysis.R
+++ genderAnalysis.R
@@ -12,6 +12,6 @@ rHelp[is.na(rHelp$gender), "gender"] <- "unknown"
 rHelp$date <- as.Date(paste(rHelp$year, rHelp$month, '01', sep = '-'), "%Y-%b-%d")
 
 rHelp_sum <- rHelp %.% group_by(date, gender) %.% summarise(r_helpers = n())
-plot_ <- ggplot(rHelp_sum, aes(y = r_helpers, x = date, color = gender)) + geom_line() + ggtitle("Number of unique (within month) posters to r-help, by gender")
+plot_ <- ggplot(rHelp_sum, aes(y = r_helpers, x = date, color = gender)) + geom_line() + ggtitle("Number of unique (within month)\nposters to r-help, by gender")
 
 print(plot_)
